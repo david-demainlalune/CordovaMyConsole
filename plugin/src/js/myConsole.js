eb0ba44ca6ef6c1656dd3b8e426b91d2c90e6323
david hodgetts <david.demainlalune@gmail.com>
2014-11-26 11:12:27 +0100

    cleanup javascript

--- plugin/src/js/myConsole.js
+++ plugin/src/js/myConsole.js
@@ -1,7 +1,6 @@
 var exec = cordova.require('cordova/exec');
 
 window.log = function(string){
-    console.log("youpi");
     var success = function(){};
     var error = function(){};
     exec(success,
